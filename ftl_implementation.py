013255ae5353460c0ce7bebc68fd7f39adde6d6c
tstan914 <tstan914@gmail.com>
2019-03-27 21:42:05 -0700

    Added comms build 0

--- ftl_implementation.py
+++ ftl_implementation.py
@@ -7,11 +7,13 @@ Tristan Sherman
 
 Cool stuff UAV Swarm Cal Poly Pomona 2019
 """
-
+from __future__ import print_function
 import ftl_definitions
 from ftl_definitions import *
 import time
-
+from struct import *
+from RF24 import *
+from RF24Network import *
 import sys
 import dronekit
 from dronekit import connect, VehicleMode, LocationGlobalRelative, LocationGlobal, Command
@@ -23,6 +25,35 @@ vehicle = connect('/dev/serial0', wait_ready=True, baud = 57600)
 # Send ack of successful connect
 # ---------- TODO -----------------
 # Tristan Cady: make a function that sends an ack
+
+# Radio Configuration
+# Configure Radio SPI Connections
+radio = RF24(25,0)
+network = RF24Network(radio)
+
+# Octal
+octlit = lambda n:int(n, 8)
+
+# Pull information about network address
+lines = [line.rstrip('\n') for line in open('sid.txt')]
+
+l_sid = lines[0]
+o_sid = lines[1]
+
+this_node = octlit(l_sid)
+other_node = octlit(o_sid)
+print('This Node: ', this_node, ' Other Node: ', other_node)
+
+radio.begin()
+time.sleep(0.1)
+
+# Open network on channel 90
+network.begin(90, this_node)
+radio.printDetails()
+
+# Initialize packet info
+packets_sent = 0
+packets_recieved = 0
 
 # Wait for toggle from aircraft
 # Takeoff to 10m
@@ -45,8 +76,12 @@ boid[1] = Boid('otherBoid', [ 0, 1, 0], [0, 0, 0])
 while True:
     
     if safetySw > 1500:
-        
-        #Calculatie new position      
+
+        # Update network
+        network.update()
+        print('Packets Sent: ', packets_sent, ' Packets Recieved: ', packets_recieved)
+
+        # Calculatie new position
         # Get velocity and position of the controlled boid
         boid[0].velocity = vehicle.velocity
         boid[0].position = [vehicle.location.global_relative_frame.lat, vehicle.location.global_relative_frame.lon, vehicle.location.global_relative_frame.alt] 
@@ -54,7 +89,14 @@ while True:
         print(boid[0].position[1])
         # Obtain the heading value of the controlled boid
         boid[0].hdg = vehicle.heading
-        
+
+        payload = pack('<ffff', boid[0].position[0], boid[0].position[1], boid[0].position[2], boid[0].hdg)
+        ok = network.write(RF24NetworkHeader(other_node), payload)
+        if ok:
+            print('Successfully Sent Data')
+            packets_sent += 1
+        else:
+            print('FAILED To Send Data')
     
         # ---------------------- TODO ------------------------
         # EVENTUALLY: Determine which boid is the local leader
@@ -63,8 +105,16 @@ while True:
         #----------------------- TODO --------------------------
         # TRISTAN CADY: Obtain position, velocity, and heading of other drone
         # Obtain position, velocity and heading of the other boid
-        
-        
+        while network.available():
+            header, o_payload = network.read(12)
+            lat, lon, alt, hdg = unpack('<ffff', bytes(o_payload))
+            print('Recieved Payload', ' Lat: ', lat, ' Lon: ', lon, ' Alt:', alt, ' Hdg: ', hdg, ' | From ', oct(header.from_node))
+            boid[1].position[0] = lat
+            boid[1].position[1] = lon
+            boid[1].position[2] = alt
+            boid[1].hdg = hdg
+            packets_recieved += 1
+
         #Calculate steering vector for the boid
         cohesionVector = ftl_definitions.calculateCohesionVector(boid,0)
         separationVector = ftl_definitions.calculateSeparationVector(boid,0)
